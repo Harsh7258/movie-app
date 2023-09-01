cd3fb15340f58be539d44348d93269601a88f46f
K Harsh <harshnaidu2u@gmail.com>
2023-09-01 12:05:55 +0530

    new files created and API fetched

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import MovieCard from './movieCard';
+
+import searchSVG from './search.svg';
+
+// a917e62
+
+const API_URL = 'http://www.omdbapi.com/?apikey=ba917e62&';
 
 const App = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [searchMov, setSearchMov] = useState('');
+
+  const searchMovies = async (title) => {
+    const res = await fetch(`${API_URL}s=${title}`);
+    const data = await res.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies('superman');
+  }, []);
+
   return (
-    <div className="App">
-      react
+    <div className="app">
+      <h1>Movie App</h1>
+      <div className='search'>
+        <input placeholder="Search for movies"
+        value={searchMov}
+        onChange={(input) => setSearchMov(input.target.value)}/>
+
+        <img src={searchSVG}
+        alt='search'
+        onClick={() => searchMovies(searchMov)}/>
+      </div>
+
+      {
+         movies?.length > 0 
+         ? 
+           (<div className="container">
+             {movies.map((mov) => (
+              <MovieCard mov={mov} />
+             ))}
+           </div>) 
+         :
+           (<div className="empty">
+             <h2>No  movies Found</h2>
+           </div>)
+      };
     </div>
   );
 }
